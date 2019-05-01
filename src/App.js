1227839a67c8dc5ff8c689298bb2145d26159e01
Amanda Sambawa <amanda.sambawa@alaskaair.com>
2019-05-01 11:33:56 -0700

    Add time limitations and warning

--- src/App.js
+++ src/App.js
@@ -8,12 +8,12 @@ import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogContent';
 import DialogContent from '@material-ui/core/DialogContentText';
 import DialogContentText from '@material-ui/core/DialogContentText';
-import DialogTitle from '@material-ui/core/DialogTitle';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      inputValue: 10,
       totalSeconds: 600, 
       minutes: 10,
       seconds: 0,
@@ -26,6 +26,13 @@ class App extends Component {
     this.stopTimer = this.stopTimer.bind(this);
     this.resetTime = this.resetTime.bind(this);
     this.tick = this.tick.bind(this);
+    this.handleClose = this.handleClose.bind(this);
+  }
+
+  handleClose() {
+    this.setState({
+      open: false
+    });
   }
 
   tick() {
@@ -46,19 +53,22 @@ class App extends Component {
       seconds: currSeconds,
       totalSeconds: this.state.totalSeconds - 1,
       formattedSeconds: currForSeconds,
-    })
+    });
   }
 
   changeTime(event) {
     var total = event.target.value * 60;
-    if (event.target.value > 0) {
+    if (event.target.value > 0 && event.target.value < 60) {
       this.setState({
+        inputValue: event.target.value,
         minutes: event.target.value,
         totalSeconds: total,
         currentInput: event.target.value,
       })
     } else {
-
+      this.setState({
+        open: true
+      })
     }
   }
 
@@ -90,23 +100,44 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header" height="10">
-          {/* <img src={logo} className="App-logo" alt="logo" /> */}
           <h3>AS Mob Timer </h3>
-        <div>
-          <div style={{marginBottom: 10}}>
-            <h3>Input your desired time (minutes):</h3>
-            <input type="number" min="1" onChange={this.changeTime}></input>
-            <h1>{this.state.minutes} : {this.state.formattedSeconds}</h1>
-          </div>
           <div>
+            <div style={{marginBottom: 10}}>
+              <h3>Input your desired time (minutes):</h3>
+              <TextField
+                value={this.state.inputValue}
+                onChange={this.changeTime}
+                type="number"
+                margin="normal"
+                style={{backgroundColor: 'white'}}
+              />
+              <h1>{this.state.minutes} : {this.state.formattedSeconds}</h1>
+            </div>
+            <div>
             <Button variant="contained" onClick={this.startTimer} color="primary" style={{marginLeft: 10, marginRight: 10}}>Start</Button>
             <Button variant="contained" onClick={this.stopTimer} color="secondary" style={{marginLeft: 10, marginRight: 10}}>Stop</Button>
             <Button variant="contained" onClick={this.resetTime} style={{marginLeft: 10, marginRight: 10}}>Reset</Button>
+            </div>
           </div>
-          <Dialog>
+        </header>
+        <div>
+        <Dialog
+          open={this.state.open}
+          onClose={this.handleClose} 
+          aria-describedby="alert-dialog-description"
+        >
+          <DialogContent style={{ margin: "1em"}}>
+            <DialogContentText id="alert-dialog-description">
+              Please enter a number between 1 and 60.
+            </DialogContentText>
+          </DialogContent>
+          {/* <DialogActions> */}
+            <Button onClick={this.handleClose} color="primary">
+              Okay
+            </Button>
+          {/* </DialogActions> */}
           </Dialog> 
         </div>
-        </header>
       </div>
     );
   }
